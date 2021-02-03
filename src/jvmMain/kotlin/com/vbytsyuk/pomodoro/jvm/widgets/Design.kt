f3f26f4dae6164dc75f8fa258ccace74e43a935e
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2021-02-03 22:44:49 +0300

    Implemented Colors

--- src/jvmMain/kotlin/com/vbytsyuk/pomodoro/jvm/widgets/Design.kt
+++ src/jvmMain/kotlin/com/vbytsyuk/pomodoro/jvm/widgets/Design.kt
@@ -4,9 +4,41 @@ import javafx.scene.paint.Color
 import tornadofx.*
 
 
-object Colors {
-    val red: Color = c("#CC2936")
-    val green: Color = c("#47A025")
-    val white: Color = c("#F6F7F8")
-    val black: Color = c("#153131")
+interface Colors {
+    val white: Color
+    val black: Color
+    val grey: Color
+    val blue: Color
+    val greyBlue: Color
+    val red: Color
+    val yellow: Color
+    val green: Color
+    val cyan: Color
+    val violet: Color
+}
+
+object LightColors : Colors {
+    override val white = c("#F5F6FA")
+    override val black = c("#353B48")
+    override val grey = c("#7F8FA6")
+    override val blue = c("#273C75")
+    override val greyBlue = c("#487EB0")
+    override val red = c("#E84118")
+    override val yellow = c("#FBC531")
+    override val green = c("#4CD137")
+    override val cyan = c("#00A8FF")
+    override val violet = c("#9C88FF")
+}
+
+object DarkColors : Colors {
+    override val white = c("#DCDDE1")
+    override val black = c("#2F3640")
+    override val grey = c("#718093")
+    override val blue = c("#192A56")
+    override val greyBlue = c("#40739E")
+    override val red = c("#C23616")
+    override val yellow = c("#E1B12C")
+    override val green = c("#44BD32")
+    override val cyan = c("#0097E6")
+    override val violet = c("#8C7AE6")
 }
